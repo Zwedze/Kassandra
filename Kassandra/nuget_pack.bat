5cf4a48f1eb4af70c17583d06a1d47c5c57f9c3a
Esteban Goffaux <estebangoffaux@live.se>
2015-06-08 01:17:23 +0200

    Add nuget package configuration files

---
+++ Kassandra/nuget_pack.bat
@@ -0,0 +1,2 @@
+nuget pack Kassandra.Core\Kassandra.Core.csproj -Prop Configuration=Release
+nuget pack Kassandra.Connector.Sql\Kassandra.Connector.Sql.csproj -Prop Configuration=Release -IncludeReferencedProjects
